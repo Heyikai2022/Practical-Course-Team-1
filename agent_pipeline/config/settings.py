6ca67f004b794137d85be7457186e21567d0b659
Muqiu Wang <wangm@cip.ifi.lmu.de>
2025-07-04 02:55:41 +0200

    CORE_AGENT and TARGET_OPENAI added

--- agent_pipeline/config/settings.py
+++ agent_pipeline/config/settings.py
@@ -3,14 +3,19 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# === API KEYS ===
+# === API URL & KEYS ===
 NOVITA_API_KEY = os.getenv("NOVITA_API_KEY")
 NOVITA_API_URL = os.getenv("NOVITA_API_URL")
+
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 # === Models ===
-PROMPT_REFINER_MODEL = "gemini-1.5-pro-latest"
-TARGET_MODEL_NAME = "qwen/qwen2.5-7b-instruct" # Edit the target model here manually
+CORE_AGENT = "gemini" # or "openai"
+
+PROMPT_REFINER_MODEL = "gemini-1.5-pro-latest" # or "gpt-3.5-turbo"
+TARGET_MODEL_NAME = "" # Edit the target model here manually
 
 # === Experiment ===
 REASON = True # Edit REASON manually to test different prompts
+TARGET_OPENAI = False # Set TARGET_OPENAI manually to True, if the tested target model is from GPT series
